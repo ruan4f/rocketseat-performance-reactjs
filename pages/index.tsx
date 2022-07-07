0dbd3c8bb1aaf4122c3bf8f0927a3768fe502fd2
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-07-07 13:02:04 -0300

    New components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,37 @@
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/SearchResults";
 
 export default function Home() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q:${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
   )
 }
